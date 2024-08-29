9dbce82c9298dbe6635beaf76f884cdecae2f384
Zrazhevskii <vbn-134@yandex.ru>
2024-08-29 15:26:43 +0300

    создание структуры

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,37 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'airbnb',
+        // 'eslint:recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:prettier/recommended',
     ],
-  },
-}
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh', 'react', 'import', 'jsx-a11y'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'alway',
+            },
+        ],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'jsx-a11y/no-autofocus': 'off',
+        'no-alert': 'off',
+        'react/require-default-props': 0,
+    },
+};
